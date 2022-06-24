6b7e7a1a1adcba249e8b422ae0e5a96b9e0d9bb5
Arif Er <arifer612@pm.me>
2022-06-24 17:50:09 +0800

    style: 4 spaces for tab indents in shell scripts and files
    This will match the indentations with files in the events directory

--- source/compose.manager/scripts/compose.sh
+++ source/compose.manager/scripts/compose.sh
@@ -4,31 +4,31 @@ export HOME=/root
 
 case $1 in
 
-  up)
-    docker compose -f "$2" -p "$3" up -d 2>&1
-    ;;
+    up)
+        docker compose -f "$2" -p "$3" up -d 2>&1
+        ;;
 
-  down)
-    docker compose -f "$2" -p "$3" down  2>&1
-    ;;
+    down)
+        docker compose -f "$2" -p "$3" down  2>&1
+        ;;
 
-  pull)
-    docker compose -f "$2" -p "$3" pull  2>&1
-    ;;
+    pull)
+        docker compose -f "$2" -p "$3" pull  2>&1
+        ;;
 
-  stop)
-    docker compose -f "$2" -p "$3" stop  2>&1
-    ;;
+    stop)
+        docker compose -f "$2" -p "$3" stop  2>&1
+        ;;
 
-  list) 
-    docker compose ls -a --format json 2>&1
-    ;;
+    list)
+        docker compose ls -a --format json 2>&1
+        ;;
 
-  logs)
-    docker compose -f "$2" logs -f 2>&1
-    ;;
+    logs)
+        docker compose -f "$2" logs -f 2>&1
+        ;;
 
-  *)
-    echo -n "unknown command"
-    ;;
+    *)
+        echo -n "unknown command"
+        ;;
 esac
